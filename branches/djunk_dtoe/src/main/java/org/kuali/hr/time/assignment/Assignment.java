e0bd1d5b55055bfc5d00c8df250f4a54365ea2fb
mpham <mpham@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-06-24 21:30:57 +0000

    recovered only djunk_dtoe

---
+++ branches/djunk_dtoe/src/main/java/org/kuali/hr/time/assignment/Assignment.java
@@ -0,0 +1,130 @@
+package org.kuali.hr.time.assignment;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.LinkedHashMap;
+
+import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
+
+public class Assignment extends PersistableBusinessObjectBase {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -3408305833805778653L;
+    
+    private Long assignmentId;
+    private String principalId;
+    private Integer jobNumber;
+    private Date effectiveDate;
+    private String earnCode;
+    private Integer effectiveSequence;
+    private Integer workAreaId;
+    private Integer taskId;
+    private BigDecimal percent;
+    private boolean active;
+    
+    public Assignment() {
+	
+    }
+    
+    public Assignment(String principalId, Integer jobNumber, Date effectiveDate, String earnCode, Integer effectiveSequence, Integer workAreaId, Integer taskId, BigDecimal percent) {
+	this.principalId = principalId;
+	this.jobNumber = jobNumber;
+	this.effectiveDate = effectiveDate;
+	this.earnCode = earnCode;
+	this.effectiveSequence = effectiveSequence;
+	this.workAreaId = workAreaId;
+	this.taskId = taskId;
+	this.percent = percent;
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    protected LinkedHashMap toStringMapper() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+
+
+    public String getPrincipalId() {
+        return principalId;
+    }
+
+    public void setPrincipalId(String principalId) {
+        this.principalId = principalId;
+    }
+
+    public Integer getJobNumber() {
+        return jobNumber;
+    }
+
+    public void setJobNumber(Integer jobNumber) {
+        this.jobNumber = jobNumber;
+    }
+
+    public Date getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(Date effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public String getEarnCode() {
+        return earnCode;
+    }
+
+    public void setEarnCode(String earnCode) {
+        this.earnCode = earnCode;
+    }
+
+    public Integer getEffectiveSequence() {
+        return effectiveSequence;
+    }
+
+    public void setEffectiveSequence(Integer effectiveSequence) {
+        this.effectiveSequence = effectiveSequence;
+    }
+
+    public Integer getWorkAreaId() {
+        return workAreaId;
+    }
+
+    public void setWorkAreaId(Integer workAreaId) {
+        this.workAreaId = workAreaId;
+    }
+
+    public Integer getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
+
+    public BigDecimal getPercent() {
+        return percent;
+    }
+
+    public void setPercent(BigDecimal percent) {
+        this.percent = percent;
+    }
+
+    public Long getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(Long assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ branches/djunk_dtoe/src/main/webapp/WEB-INF/jsp/maintenance/ClockLocationRuleMaintenance.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java"%>
+<%@ taglib tagdir="/WEB-INF/tags/shared" prefix="hr"%>
+
+<hr:page>
+	<hr:listMaintenance nestingPath="rule" 
+	columns="department,workArea,universityId,employeeRecord,ipAddress" tabFooter="true"/>
+</hr:page>
