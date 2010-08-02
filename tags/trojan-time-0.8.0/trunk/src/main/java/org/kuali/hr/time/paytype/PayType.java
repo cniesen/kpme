93dcc7da649d264fe1d8f1d1c5311da7e2afc658
crivera <crivera@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-08-02 20:22:00 +0000

    [maven-release-plugin]  copy for tag trojan-time-0.8.0

---
+++ tags/trojan-time-0.8.0/trunk/src/main/java/org/kuali/hr/time/paytype/PayType.java
@@ -0,0 +1,116 @@
+package org.kuali.hr.time.paytype;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.LinkedHashMap;
+
+import org.kuali.hr.time.holidaycalendar.HolidayCalendar;
+import org.kuali.hr.time.paycalendar.PayCalendar;
+import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
+
+public class PayType extends PersistableBusinessObjectBase {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long payTypeId;
+	private String payType;
+	private String descr;
+	private String calendarGroup;
+	private String regEarnCode;
+	private Date effectiveDate;
+	private Time timestamp;
+	private String holidayCalendarGroup;
+
+	private HolidayCalendar holidayCalendarGroupObj;
+	private PayCalendar calendarGroupObj;
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	protected LinkedHashMap toStringMapper() {
+		return null;
+	}
+
+	public Long getPayTypeId() {
+		return payTypeId;
+	}
+
+	public void setPayTypeId(Long payTypeId) {
+		this.payTypeId = payTypeId;
+	}
+
+	public String getPayType() {
+		return payType;
+	}
+
+	public void setPayType(String payType) {
+		this.payType = payType;
+	}
+
+	public String getDescr() {
+		return descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+
+	public String getCalendarGroup() {
+		return calendarGroup;
+	}
+
+	public void setCalendarGroup(String calendarGroup) {
+		this.calendarGroup = calendarGroup;
+	}
+
+	public String getRegEarnCode() {
+		return regEarnCode;
+	}
+
+	public void setRegEarnCode(String regEarnCode) {
+		this.regEarnCode = regEarnCode;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public void setTimestamp(Time timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Time getTimestamp() {
+		return timestamp;
+	}
+
+	public void setHolidayCalendarGroup(String holidayCalendarGroup) {
+		this.holidayCalendarGroup = holidayCalendarGroup;
+	}
+
+	public String getHolidayCalendarGroup() {
+		return holidayCalendarGroup;
+	}
+
+	public void setHolidayCalendarGroupObj(HolidayCalendar holidayCalendarGroupObj) {
+		this.holidayCalendarGroupObj = holidayCalendarGroupObj;
+	}
+
+	public HolidayCalendar getHolidayCalendarGroupObj() {
+		return holidayCalendarGroupObj;
+	}
+
+	public void setCalendarGroupObj(PayCalendar calendarGroupObj) {
+		this.calendarGroupObj = calendarGroupObj;
+	}
+
+	public PayCalendar getCalendarGroupObj() {
+		return calendarGroupObj;
+	}
+
+	
+}
