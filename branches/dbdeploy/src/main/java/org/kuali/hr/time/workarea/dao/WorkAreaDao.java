a65448db77e3548d4e4a9ebe8456ba724e5fea03
crivera <crivera@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-09-07 22:01:30 +0000

    not needed

--- branches/dbdeploy/src/main/java/org/kuali/hr/time/workarea/dao/WorkAreaDao.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.hr.time.workarea.dao;
-
-import org.kuali.hr.time.workarea.WorkArea;
-
-public interface WorkAreaDao {
-
-    public WorkArea getWorkArea(Long workAreaId);
-    public void saveOrUpdate(WorkArea workArea);
-}
