cef73c925dd493cdc016a145101ca0145038cb34
crivera <crivera@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-09-23 15:50:52 +0000

    not needed

--- tags/trojan-time-0.3.0/src/main/java/org/kuali/hr/time/timeblock/service/TimeBlockService.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.hr.time.timeblock.service;
-
-import org.kuali.hr.time.timeblock.TimeBlock;
-
-public interface TimeBlockService {
-
-	public void saveTimeBlock(TimeBlock timeBlock);
-}
