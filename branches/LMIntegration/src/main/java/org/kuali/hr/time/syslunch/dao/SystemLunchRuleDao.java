bb0721fc89b663ea70a836b430a26e6f3379b5e2
crivera <crivera@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-10-27 16:53:41 +0000

    not needed

--- branches/LMIntegration/src/main/java/org/kuali/hr/time/syslunch/dao/SystemLunchRuleDao.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.hr.time.syslunch.dao;
-
-import java.sql.Date;
-
-import org.kuali.hr.time.syslunch.rule.SystemLunchRule;
-
-public interface SystemLunchRuleDao {
-	public SystemLunchRule getSystemLunchRule(Date asOfDate);
-}
--- branches/LMIntegration/src/main/java/org/kuali/hr/time/workflow/service/DocumentService.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.hr.time.workflow.service;
-
-import org.kuali.rice.kew.service.WorkflowDocument;
-
-public interface DocumentService {
-
-	public WorkflowDocument createWorkflowDocument(String principalId, String documentType, String title);
-}
