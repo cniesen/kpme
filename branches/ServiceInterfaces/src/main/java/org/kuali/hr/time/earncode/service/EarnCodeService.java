c4eddc17fb12739ae95667ac4581dd80b3167561
smok <smok@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-11-18 21:59:57 +0000

    sm -T2-277 11.18.2010 Add webService

--- branches/ServiceInterfaces/src/main/java/org/kuali/hr/time/earncode/service/EarnCodeService.java
+++ branches/ServiceInterfaces/src/main/java/org/kuali/hr/time/earncode/service/EarnCodeService.java
@@ -3,12 +3,31 @@ package org.kuali.hr.time.earncode.service;
 import java.sql.Date;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
 import org.kuali.hr.time.assignment.Assignment;
 import org.kuali.hr.time.earncode.EarnCode;
+import org.kuali.hr.time.exceptions.TkException;
+
+@WebService(name = "earnCodeServiceSOAP", targetNamespace = "/tk")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 
 public interface EarnCodeService {
 
+	@WebMethod(exclude = true)
 	public List<EarnCode> getEarnCodes(Assignment a);
 	
+	@WebMethod(exclude = true)
 	public EarnCode getEarnCode(String earnCode, Date asOfDate);
+	
+	
+	// WebService
+	@WebMethod
+	public boolean addEarnCodes(
+			@WebParam(name = "earnCode") List<EarnCode> earnCodes) throws TkException;
+	
+	
 }
