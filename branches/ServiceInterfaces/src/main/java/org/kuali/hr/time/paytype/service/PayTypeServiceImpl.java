62d8cf85591f572d20acbc859837107ab00158a4
dbeachler <dbeachler@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-11-19 22:29:14 +0000

    djb 11.19.10 T2-278 Create Web Services

--- branches/ServiceInterfaces/src/main/java/org/kuali/hr/time/paytype/service/PayTypeServiceImpl.java
+++ branches/ServiceInterfaces/src/main/java/org/kuali/hr/time/paytype/service/PayTypeServiceImpl.java
@@ -3,43 +3,81 @@ package org.kuali.hr.time.paytype.service;
 import java.sql.Date;
 import java.util.List;
 
+import javax.jws.WebService;
+
+import org.kuali.hr.sys.context.SpringContext;
+import org.kuali.hr.time.exceptions.TkException;
 import org.kuali.hr.time.paycalendar.PayCalendar;
 import org.kuali.hr.time.paycalendar.service.PayCalendarService;
 import org.kuali.hr.time.paytype.PayType;
 import org.kuali.hr.time.paytype.dao.PayTypeDao;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
+/**
+ * @author djb - add webservices
+ * 
+ */
+@WebService(endpointInterface = "org.kuali.hr.time.paytype.service.PayTypeService")
 public class PayTypeServiceImpl implements PayTypeService {
 
-	private PayCalendarService payCalendarService;
-	private PayTypeDao payTypeDao;
-
+	
 	@Override
 	public void saveOrUpdate(PayType payType) {
-		payTypeDao.saveOrUpdate(payType);
+		SpringContext.getBean(PayTypeDao.class).saveOrUpdate(payType);
 	}
 
-	@Override
+	@Transactional
 	public void saveOrUpdate(List<PayType> payTypeList) {
-		payTypeDao.saveOrUpdate(payTypeList);
+		SpringContext.getBean(PayTypeDao.class).saveOrUpdate(payTypeList);
 	}
 
-	public void setPayTypeDao(PayTypeDao payTypeDao) {
-		this.payTypeDao = payTypeDao;
-	}
-
-	@Override
+		@Override
 	public PayType getPayType(String payType, Date effectiveDate) {
 		PayType payTypeObj = null;
-		payTypeObj = payTypeDao.getPayType(payType, effectiveDate);
+		payTypeObj = SpringContext.getBean(PayTypeDao.class).getPayType(payType, effectiveDate);
 		if (payTypeObj != null) {
-			PayCalendar payCalendar = payCalendarService.getPayCalendarByGroup(payTypeObj.getCalendarGroup());
+			PayCalendar payCalendar = SpringContext.getBean(PayCalendarService.class).getPayCalendarByGroup(payTypeObj.getCalendarGroup());
 			payTypeObj.setPayCalendar(payCalendar);
 		}
 		return payTypeObj;
 	}
 
-	public void setPayCalendarService(PayCalendarService payCalendarService) {
-		this.payCalendarService = payCalendarService;
+	// djb 11.17.10 webService
+	@Transactional	
+	public boolean addPayTypes(List<PayType> payTypes) 	throws TkException{
+		// go through each PayType that is coming in
+		// add PayType if it is NOT there.
+		for (PayType newPayType : payTypes) {
+			try {
+				PayTypeDao payTypeDao = SpringContext.getBean(PayTypeDao.class);
+				
+			    	// check if we have a PayType with the same id already
+				PayType oldPayType = payTypeDao.getPayType(newPayType.getPayType(), new Date(new java.util.Date().getTime()));
+					// if no .. save it
+				if (oldPayType == null) {
+					KNSServiceLocator.getBusinessObjectDao().save(newPayType);
+				} else {
+					// else update it
+					oldPayType.setPayType		(newPayType.getPayType());
+					oldPayType.setDescr			(newPayType.getDescr());
+					oldPayType.setCalendarGroup (newPayType.getCalendarGroup());
+					oldPayType.setRegEarnCode	(newPayType.getRegEarnCode());
+					oldPayType.setEffectiveDate	(newPayType.getEffectiveDate());
+					oldPayType.setTimestamp		(newPayType.getTimestamp());
+					oldPayType.setHolidayCalendarGroup(newPayType.getHolidayCalendarGroup());
+					oldPayType.setActive		(newPayType.getActive());
+									
+					KNSServiceLocator.getBusinessObjectDao().save(oldPayType);
+				}
+				// any exceptions will be transformed into service exceptions
+			} catch (Exception e) {
+				TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+				throw new TkException(e.getMessage());
+			}
+		}
+		return true;
+			
 	}
-
 }
