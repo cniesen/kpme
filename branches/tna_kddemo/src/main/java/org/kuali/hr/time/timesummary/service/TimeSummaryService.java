772d1d1b39494a585672f35ef8ab23b972c8a9d8
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-05-04 20:21:50 +0000

    obsolete branch cleanup

--- branches/tna_kddemo/src/main/java/org/kuali/hr/time/timesummary/service/TimeSummaryService.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.hr.time.timesummary.service;
-
-import org.kuali.hr.time.timesheet.TimesheetDocument;
-import org.kuali.hr.time.timesummary.TimeSummary;
-
-public interface TimeSummaryService {
-	public TimeSummary getTimeSummary(TimesheetDocument timesheetDocument);
-}
--- branches/tna_kddemo/src/main/java/org/kuali/hr/time/util/TagSupport.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.hr.time.util;
-
-import org.apache.log4j.Logger;
-
-public class TagSupport {
-
-	private static final Logger LOG = Logger.getLogger(TagSupport.class);
-}
