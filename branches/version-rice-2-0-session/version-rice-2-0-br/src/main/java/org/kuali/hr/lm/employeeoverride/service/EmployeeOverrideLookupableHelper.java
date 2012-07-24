756821ca50d7d7c9838f85652366a10a766c0c01
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-07-24 15:26:42 +0000

    Removing bad branch

--- branches/version-rice-2-0-session/version-rice-2-0-br/src/main/java/org/kuali/hr/lm/employeeoverride/service/EmployeeOverrideLookupableHelper.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.hr.lm.employeeoverride.service;
-
-import org.kuali.hr.time.HrEffectiveDateActiveLookupableHelper;
-
-public class EmployeeOverrideLookupableHelper extends HrEffectiveDateActiveLookupableHelper  {
-
-}
--- branches/version-rice-2-0-session/version-rice-2-0-br/src/test/config/sql/RoleTimesheetWebIntegrationTest-cleanup.sql
+++
@@ -1,1 +0,0 @@
-delete from hr_principal_attributes_t where principal_id in ('testuser1','testuser2','testuser3','testuser4','testuser5','testuser6','fran','frank','edna','fred');
