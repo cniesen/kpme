726a85d3216ee2ef3a09a0eb45839a6f306eedc4
smoaddel <smoaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-09-14 21:15:24 +0000

    post-merge corrections.

--- branches/version-1-2/db_diff_script.sql
+++ branches/version-1-2/db_diff_script.sql
@@ -9,9 +9,9 @@ ALTER TABLE `tk`.`tk_work_area_t` CHANGE COLUMN `DEFAULT_OVERTIME_EARNCODE` `DEF
 
 ALTER TABLE `tk`.`hr_job_t` ADD COLUMN `flsa_status` VARCHAR(15) NULL  AFTER `active` ;
 
-ALTER TABLE `tk`.`tk_time_sheet_init_t` CHANGE COLUMN `py_calendar_entries_id` `hr_calendar_entry_id` VARCHAR(60) NOT NULL DEFAULT '';
+-- ALTER TABLE `tk`.`tk_time_sheet_init_t` CHANGE COLUMN `py_calendar_entries_id` `hr_calendar_entry_id` VARCHAR(60) NOT NULL DEFAULT '';
 
-ALTER TABLE `tk`.`tk_time_sheet_init_t` CHANGE COLUMN `py_calendar_group` `calendar_name` VARCHAR(30) NOT NULL DEFAULT '';
+-- ALTER TABLE `tk`.`tk_time_sheet_init_t` CHANGE COLUMN `py_calendar_group` `calendar_name` VARCHAR(30) NOT NULL DEFAULT '';
 
 ALTER TABLE `tk`.`hr_earn_code_t` CHANGE COLUMN `EARN_CODE` `EARN_CODE` VARCHAR(15) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL  ;
 
@@ -49,22 +49,22 @@ ALTER TABLE `tk`.`tk_hour_detail_t` CHANGE COLUMN `earn_code` `earn_code` VARCHA
 
 DROP TABLE IF EXISTS `hr_principal_calendar_t`;
 
-CREATE TABLE `hr_principal_calendar_t` (
-  `hr_principal_attribute_id` varchar(60) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `principal_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `pay_calendar` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `timezone` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `EFFDT` date NOT NULL DEFAULT '0000-00-00',  
-  `service_date` date NOT NULL DEFAULT '0000-00-00',
-  `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `active` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT 'Y',
-  `fmla_eligible` varchar(1),
-  `workmansCompEligible` varchar(1), 
-  `OBJ_ID` varchar(36) COLLATE utf8_bin NOT NULL,
-  `VER_NBR` decimal(8,0) NOT NULL DEFAULT '1',
-  `holiday_calendar_group` varchar(3) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`hr_principal_attribute_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- CREATE TABLE `hr_principal_calendar_t` (
+--   `hr_principal_attribute_id` varchar(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+--   `principal_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+--   `pay_calendar` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '',
+--   `timezone` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+--   `EFFDT` date NOT NULL DEFAULT '0000-00-00',
+--   `service_date` date NOT NULL DEFAULT '0000-00-00',
+--   `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `active` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT 'Y',
+--   `fmla_eligible` varchar(1),
+--   `workmansCompEligible` varchar(1),
+--   `OBJ_ID` varchar(36) COLLATE utf8_bin NOT NULL,
+--   `VER_NBR` decimal(8,0) NOT NULL DEFAULT '1',
+--   `holiday_calendar_group` varchar(3) COLLATE utf8_bin DEFAULT NULL,
+--   PRIMARY KEY (`hr_principal_attribute_id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 DROP TABLE IF EXISTS `hr_principal_attributes_t`;
 
@@ -80,22 +80,19 @@ CREATE  TABLE `tk`.`hr_principal_attributes_t` (
   `fmla_eligible` VARCHAR(1) NULL ,
   `worksman_eligible` VARCHAR(1) NULL ,
   `holiday_calendar_group` VARCHAR(3) NULL ,
-  `hr_principal_attributes_tcol` VARCHAR(45) NOT NULL ,
   `OBJ_ID` VARCHAR(36) NULL ,
   PRIMARY KEY (`hr_principal_attribute_id`) );
 
 ALTER TABLE `tk`.`hr_principal_attributes_t` ADD COLUMN `ver_nbr` BIGINT NULL  AFTER `OBJ_ID` 
 , ADD INDEX `OBJ_ID` (`OBJ_ID` ASC) ;
 
-ALTER TABLE `tk`.`hr_earn_code_security_t` ADD COLUMN `sal_group` VARCHAR(20) ;
+-- ALTER TABLE `tk`.`hr_earn_code_security_t` ADD COLUMN `sal_group` VARCHAR(20) ;
 
-ALTER TABLE `tk`.`tk_batch_job_t` CHANGE COLUMN `hr_py_calendar_entry_id` `hr_calendar_entry_id` VARCHAR(15) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL;
+-- ALTER TABLE `tk`.`tk_batch_job_t` CHANGE COLUMN `hr_py_calendar_entry_id` `hr_calendar_entry_id` VARCHAR(15) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL;
 
-ALTER TABLE `tk`.`tk_batch_job_entry_t` CHANGE COLUMN `HR_PY_CALENDAR_ENTRY_ID` `HR_CALENDAR_ENTRY_ID` VARCHAR(60) NOT NULL  ;
+-- ALTER TABLE `tk`.`tk_batch_job_entry_t` CHANGE COLUMN `HR_PY_CALENDAR_ENTRY_ID` `HR_CALENDAR_ENTRY_ID` VARCHAR(60) NOT NULL  ;
 
 ALTER TABLE `tk`.`hr_position_t` ADD COLUMN `WORK_AREA` BIGINT AFTER `TIMESTAMP` ;
-
-DROP TABLE IF EXISTS `tk`.`hr_position_s`;
 
 DROP TABLE IF EXISTS `tk`.`hr_calendar_entries_t`;
 
