f46140c3828a43c371566fb7c014f3bc6042efd0
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-10-04 17:12:20 +0000

    Tagging the 1.1 branch

---
+++ tags/time-and-attendance-1.1/src/main/java/org/kuali/hr/time/paytype/service/PayTypeLookupableHelper.java
@@ -0,0 +1,47 @@
+package org.kuali.hr.time.paytype.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.hr.time.HrEffectiveDateActiveLookupableHelper;
+import org.kuali.hr.time.paytype.PayType;
+import org.kuali.hr.time.util.TKContext;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.lookup.HtmlData;
+import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
+
+public class PayTypeLookupableHelper extends HrEffectiveDateActiveLookupableHelper {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public List<HtmlData> getCustomActionUrls(BusinessObject businessObject,
+			List pkNames) {
+		List<HtmlData> customActionUrls = super.getCustomActionUrls(
+				businessObject, pkNames);
+		if (TKContext.getUser().getCurrentRoles().isSystemAdmin() || TKContext.getUser().isGlobalViewOnly()) {
+			PayType payType = (PayType) businessObject;
+			final String className = this.getBusinessObjectClass().getName();
+			final String hrPayTypeId = payType.getHrPayTypeId();
+			HtmlData htmlData = new HtmlData() {
+
+				@Override
+				public String constructCompleteHtmlTag() {
+					return "<a target=\"_blank\" href=\"inquiry.do?businessObjectClassName="
+							+ className
+							+ "&methodToCall=start&hrPayTypeId="
+							+ hrPayTypeId + "&payType=\">view</a>";
+				}
+			};
+			customActionUrls.add(htmlData);
+		} else if (customActionUrls.size() != 0) {
+			customActionUrls.remove(0);
+		}
+		return customActionUrls;
+	}
+}
