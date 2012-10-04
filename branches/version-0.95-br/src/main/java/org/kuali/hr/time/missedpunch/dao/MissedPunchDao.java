11cbc541ae34dd009c655b4e969560ce44928ba0
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-10-04 18:10:59 +0000

    Removing old branches

--- branches/version-0.95-br/src/main/java/org/kuali/hr/time/missedpunch/dao/MissedPunchDao.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.hr.time.missedpunch.dao;
-
-import org.kuali.hr.time.missedpunch.MissedPunchDocument;
-
-public interface MissedPunchDao {
-    public MissedPunchDocument getMissedPunchByRouteHeader(String headerId);
-}
--- branches/version-0.95-br/src/main/java/org/kuali/hr/time/position/dao/PositionDao.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.hr.time.position.dao;
-
-import org.kuali.hr.time.position.Position;
-
-public interface PositionDao {
-	public Position getPosition(Long hrPositionId);
-}
