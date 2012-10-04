22bdad6203dd0e91cc36e6f04ea73a76ef40c887
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-10-04 18:10:20 +0000

    Removing old branches

--- branches/tk-impl/src/main/java/edu/iu/hr/time/logic/service/ReadyToApproveService.java
+++
@@ -1,8 +0,0 @@
-package edu.iu.hr.time.logic.service;
-
-import org.kuali.hr.time.timesheet.TimesheetDocument;
-
-public interface ReadyToApproveService {
-	public boolean isReadyToApprove(String documentId);
-	public boolean isReadyToApprove(TimesheetDocument timesheetDocument);
-}
