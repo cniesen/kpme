e83e0aa6dcc7505493b1495e4c0c0abf6447be90
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-10-04 18:09:35 +0000

    Removing old branches

--- branches/rice_upgrade_to_1_0_3/src/main/java/org/kuali/hr/time/position/dao/PositionDaoSpringOjbImpl.java
+++
@@ -1,5 +0,0 @@
-package org.kuali.hr.time.position.dao;
-
-public class PositionDaoSpringOjbImpl implements PositionDao {
-
-}
--- branches/rice_upgrade_to_1_0_3/src/main/java/org/kuali/hr/time/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.hr.time.user.service;
-
-import java.sql.Date;
-
-import org.kuali.hr.time.util.TKUser;
-
-public interface UserService {
-	public TKUser buildTkUser(String principalId, Date asOfDate);
-}
