016956a9d15e51fd5659fa04db666c18565cd3b7
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-10-09 16:28:51 +0000

    KPME-1805: Correcting merge mistake

--- branches/version-1-2/src/main/java/org/kuali/hr/time/earncode/service/EarnCodeService.java
+++ branches/version-1-2/src/main/java/org/kuali/hr/time/earncode/service/EarnCodeService.java
@@ -86,6 +86,10 @@ public interface EarnCodeService {
     @Cacheable(value=EarnCode.CACHE_NAME, key="'{getOvertimeEarnCodesStrs}' + 'asOfDate=' + #p0")
     public List<String> getOvertimeEarnCodesStrs(Date asOfDate);
 
+    //make caching by who is looking at this as it is based on that what shows up
+    //@Cacheable(value= EarnCode.CACHE_NAME, key="'a=' + #p0.getTkAssignmentId() + '|' + 'asOfDate=' + #p1")
+    public List<EarnCode> getEarnCodesForTime(Assignment a, Date asOfDate);
+
     /**
 	 * get count of earn code with give earnCode
 	 * @param earnCode
