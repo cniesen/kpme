9c8e8407476e8df83d8e8db31f4618ab12dd6a3d
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-05-16 20:07:25 +0000

    try to revert Rekha's last commit

---
+++ branches/kpme-2.0/src/main/java/org/kuali/hr/pm/positionResponsibilityOption/service/PositionResponsibilityOptionService.java
@@ -0,0 +1,11 @@
+package org.kuali.hr.pm.positionResponsibilityOption.service;
+
+import org.kuali.hr.pm.positionResponsibilityOption.PositionResponsibilityOption;
+
+public interface PositionResponsibilityOptionService {
+	
+	public PositionResponsibilityOption getPositionResponsibilityOptionById(String prOptionId);
+	
+	
+
+}
