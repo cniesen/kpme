4811dcb1ab61f9cf85d938f586f6cff4efc64a6f
jjhanso <jjhanso@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-06-03 22:24:12 +0000

    KPME-2429 - ordering of earncodegroup on time summary

--- branches/kpme-1.5/src/main/webapp/js/tk.approval.backbone.js
+++ branches/kpme-1.5/src/main/webapp/js/tk.approval.backbone.js
@@ -100,7 +100,7 @@ $(function () {
                 this.fetchTimeSummary(docId);
 
                 // Here we loop through the colleciton and insert the content to the template
-                EarnCodeSections.each(function (earnCodeSection, index) {
+                EarnCodeSections.forEach(function (earnCodeSection) {
                     $parent.after(self.template({
                         // This is the time summary rows
                         "section" : earnCodeSection.toJSON(),
