3f8ccdaec3734bacf9c8f830bf87806fe7625b88
kbtaylor <kbit@users.noreply.github.com>
2013-06-26 16:57:21 +0000

    KPME-2521: Fixing earn code fetching logic

--- src/main/java/org/kuali/hr/time/earncode/service/EarnCodeServiceImpl.java
+++ src/main/java/org/kuali/hr/time/earncode/service/EarnCodeServiceImpl.java
@@ -125,35 +125,31 @@ public class EarnCodeServiceImpl implements EarnCodeService {
                     //  make sure we got something back from the earn code dao
                     if (ec != null) {
 
-                        //  now that we have a list of security earn codes, compare their accrual categories to the user's accrual category list.
-                        //  we also allow earn codes that have no accrual category assigned.
-                        if (listAccrualCategories.contains(ec.getAccrualCategory()) || ec.getAccrualCategory() == null) {
+                        //  if the user's fmla flag is Yes, that means we are not restricting codes based on this flag, so any code is shown.
+                        //    if the fmla flag on a code is yes they can see it.    (allow)
+                        //    if the fmla flag on a code is no they should see it.  (allow)
+                        //  if the user's fmla flag is No,
+                        //    they can see any codes which are fmla=no.             (allow)
+                        //    they can not see codes with fmla=yes.                 (exclude earn code)
+                        //  the fmla earn codes=no do not require any exclusion
+                        //  the only action required is if the fmla user flag=no: exclude those codes with fmla=yes.
 
-                            //  if the user's fmla flag is Yes, that means we are not restricting codes based on this flag, so any code is shown.
-                            //    if the fmla flag on a code is yes they can see it.    (allow)
-                            //    if the fmla flag on a code is no they should see it.  (allow)
-                            //  if the user's fmla flag is No,
-                            //    they can see any codes which are fmla=no.             (allow)
-                            //    they can not see codes with fmla=yes.                 (exclude earn code)
-                            //  the fmla earn codes=no do not require any exclusion
-                            //  the only action required is if the fmla user flag=no: exclude those codes with fmla=yes.
+                        if ( (fmlaEligible || ec.getFmla().equals("N")) ) {
+                            if (ec.getAccrualCategory() == null
+                            		|| (listAccrualCategories.contains(ec.getAccrualCategory()) 
+                            				&& LMConstants.ACCRUAL_BALANCE_ACTION.USAGE.equals(ec.getAccrualBalanceAction()))) {
+                                // go on, we are allowing these three combinations: YY, YN, NN
 
-                            if ( (fmlaEligible || ec.getFmla().equals("N")) ) {
-                                //only want usage accrual balance actions
-                                if (StringUtils.equals(ec.getAccrualBalanceAction(), LMConstants.ACCRUAL_BALANCE_ACTION.USAGE)) {
-                                    // go on, we are allowing these three combinations: YY, YN, NN
+                                //  apply the same logic as FMLA to the Worker Compensation flags.
+                                if ( (workersCompEligible || ec.getWorkmansComp().equals("N")) ) {
+                                    // go on, we are allowing these three combinations: YY, YN, NN.
 
-                                    //  apply the same logic as FMLA to the Worker Compensation flags.
-                                    if ( (workersCompEligible || ec.getWorkmansComp().equals("N")) ) {
-                                        // go on, we are allowing these three combinations: YY, YN, NN.
-
-                                        //  determine if the holiday earn code should be displayed.
-                                        if ( showEarnCodeIfHoliday(ec, dec) ) {
-                                            //  non-Holiday earn code will go on, Holiday earn code must meet some requirements in the method.
-                                            if ( !StringUtils.equals(regularEarnCode.toString(), dec.getEarnCode()) ) {
-                                                //  add earn code if it is not the reg earn code.
-                                                earnCodes.add(ec);
-                                            }
+                                    //  determine if the holiday earn code should be displayed.
+                                    if ( showEarnCodeIfHoliday(ec, dec) ) {
+                                        //  non-Holiday earn code will go on, Holiday earn code must meet some requirements in the method.
+                                        if ( !StringUtils.equals(regularEarnCode.toString(), dec.getEarnCode()) ) {
+                                            //  add earn code if it is not the reg earn code.
+                                            earnCodes.add(ec);
                                         }
                                     }
                                 }
@@ -214,28 +210,24 @@ public class EarnCodeServiceImpl implements EarnCodeService {
                     //  make sure we got something back from the earn code dao
                     if (ec != null) {
 
-                        //  now that we have a list of security earn codes, compare their accrual categories to the user's accrual category list.
-                        //  we also allow earn codes that have no accrual category assigned.
-                        if (listAccrualCategories.contains(ec.getAccrualCategory()) || ec.getAccrualCategory() == null) {
+                        //  if the user's fmla flag is Yes, that means we are not restricting codes based on this flag, so any code is shown.
+                        //    if the fmla flag on a code is yes they can see it.    (allow)
+                        //    if the fmla flag on a code is no they should see it.  (allow)
+                        //  if the user's fmla flag is No,
+                        //    they can see any codes which are fmla=no.             (allow)
+                        //    they can not see codes with fmla=yes.                 (exclude earn code)
+                        //  the fmla earn codes=no do not require any exclusion
+                        //  the only action required is if the fmla user flag=no: exclude those codes with fmla=yes.
 
-                            //  if the user's fmla flag is Yes, that means we are not restricting codes based on this flag, so any code is shown.
-                            //    if the fmla flag on a code is yes they can see it.    (allow)
-                            //    if the fmla flag on a code is no they should see it.  (allow)
-                            //  if the user's fmla flag is No,
-                            //    they can see any codes which are fmla=no.             (allow)
-                            //    they can not see codes with fmla=yes.                 (exclude earn code)
-                            //  the fmla earn codes=no do not require any exclusion
-                            //  the only action required is if the fmla user flag=no: exclude those codes with fmla=yes.
-
-                            if ( (fmlaEligible || ec.getFmla().equals("N")) ) {
-                                // go on, we are allowing these three combinations: YY, YN, NN
-
-                                //  Apply the same logic as FMLA to the Worker Compensation flags.
-                                if ( (workersCompEligible || ec.getWorkmansComp().equals("N")) ) {
-                                    // go on, we are allowing these three combinations: YY, YN, NN.
-
-                                    //accrual balance action needs to be usage
-                                    if (StringUtils.equals(ec.getAccrualBalanceAction(), LMConstants.ACCRUAL_BALANCE_ACTION.USAGE)) {
+                        if ( (fmlaEligible || ec.getFmla().equals("N")) ) {
+                        	if (listAccrualCategories.contains(ec.getAccrualCategory()) || ec.getAccrualCategory() == null) {
+                                if (StringUtils.equals(ec.getAccrualBalanceAction(), LMConstants.ACCRUAL_BALANCE_ACTION.USAGE)) {
+                                	// go on, we are allowing these three combinations: YY, YN, NN.
+                                	
+		                            //  Apply the same logic as FMLA to the Worker Compensation flags.
+		                            if ( (workersCompEligible || ec.getWorkmansComp().equals("N")) ) {
+		                                // go on, we are allowing these three combinations: YY, YN, NN.
+		                            	
                                         //  now process the scheduled leave flag, but only for the Planning Calendar, not for the Reporting Calendar.
                                         //  determine if the planning calendar is in effect.
                                         if (isLeavePlanningCalendar) {
