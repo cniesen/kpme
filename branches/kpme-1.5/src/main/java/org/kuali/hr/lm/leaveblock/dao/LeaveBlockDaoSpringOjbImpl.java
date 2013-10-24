baadd59dc48580bce9d3f2781f462f4ae934ccc8
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-10-24 16:37:18 +0000

    kpme-2937: changed logic to retrieve leave blocks using correct end date for pay period

--- branches/kpme-1.5/src/main/java/org/kuali/hr/lm/leaveblock/dao/LeaveBlockDaoSpringOjbImpl.java
+++ branches/kpme-1.5/src/main/java/org/kuali/hr/lm/leaveblock/dao/LeaveBlockDaoSpringOjbImpl.java
@@ -323,7 +323,7 @@ public class LeaveBlockDaoSpringOjbImpl extends PlatformAwareDaoBaseOjb implemen
         Criteria root = new Criteria();
         root.addEqualTo("principalId", principalId);
         root.addGreaterOrEqualThan("leaveDate", beginDate);
-        root.addLessThan("leaveDate", endDate);
+        root.addLessOrEqualThan("leaveDate", endDate);
         List<String> typeValues = new ArrayList<String>();
         typeValues.add(LMConstants.LEAVE_BLOCK_TYPE.LEAVE_CALENDAR);
         typeValues.add(LMConstants.LEAVE_BLOCK_TYPE.TIME_CALENDAR);
