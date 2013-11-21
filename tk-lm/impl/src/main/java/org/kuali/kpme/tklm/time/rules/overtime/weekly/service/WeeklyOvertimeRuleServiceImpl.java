3be8bb782ca5b07152583cbf16feb0b30a938a41
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-11-21 07:07:52 +0000

    KPME-3028 : Time detail summary for semi monthly- sat not showing issue resolved

--- tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/rules/overtime/weekly/service/WeeklyOvertimeRuleServiceImpl.java
+++ tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/rules/overtime/weekly/service/WeeklyOvertimeRuleServiceImpl.java
@@ -98,7 +98,7 @@ public class WeeklyOvertimeRuleServiceImpl implements WeeklyOvertimeRuleService 
 		List<List<FlsaWeek>> flsaWeeks = new ArrayList<List<FlsaWeek>>();
 		
         DateTimeZone zone = HrServiceLocator.getTimezoneService().getUserTimezoneWithFallback();
-		List<FlsaWeek> currentWeeks = aggregate.getFlsaWeeks(zone);
+		List<FlsaWeek> currentWeeks = aggregate.getFlsaWeeks(zone, 0, false);
 		
 		for (ListIterator<FlsaWeek> weekIterator = currentWeeks.listIterator(); weekIterator.hasNext(); ) {
 			List<FlsaWeek> flsaWeek = new ArrayList<FlsaWeek>();
@@ -120,7 +120,7 @@ public class WeeklyOvertimeRuleServiceImpl implements WeeklyOvertimeRuleService 
                     if (CollectionUtils.isNotEmpty(timeBlocks)) {
 						CalendarEntry calendarEntry = HrServiceLocator.getCalendarEntryService().getCalendarDatesByPayEndDate(principalId, timesheetDocumentHeader.getEndDateTime(), HrConstants.PAY_CALENDAR_TYPE);
 						TkTimeBlockAggregate previousAggregate = new TkTimeBlockAggregate(timeBlocks, leaveBlocks, calendarEntry, calendarEntry.getCalendarObj(), true);
-						List<FlsaWeek> previousWeek = previousAggregate.getFlsaWeeks(zone);
+						List<FlsaWeek> previousWeek = previousAggregate.getFlsaWeeks(zone, 0, false);
 						if (CollectionUtils.isNotEmpty(previousWeek)) {
 							flsaWeek.add(previousWeek.get(previousWeek.size() - 1));
 						}
@@ -143,7 +143,7 @@ public class WeeklyOvertimeRuleServiceImpl implements WeeklyOvertimeRuleService 
 					if (CollectionUtils.isNotEmpty(timeBlocks)) {
 						CalendarEntry calendarEntry = HrServiceLocator.getCalendarEntryService().getCalendarDatesByPayEndDate(principalId, timesheetDocumentHeader.getEndDateTime(), HrConstants.PAY_CALENDAR_TYPE);
 						TkTimeBlockAggregate nextAggregate = new TkTimeBlockAggregate(timeBlocks, leaveBlocks, calendarEntry, calendarEntry.getCalendarObj(), true);
-						List<FlsaWeek> nextWeek = nextAggregate.getFlsaWeeks(zone);
+						List<FlsaWeek> nextWeek = nextAggregate.getFlsaWeeks(zone, 0 , false);
 						if (CollectionUtils.isNotEmpty(nextWeek)) {
 							flsaWeek.add(nextWeek.get(0));
 						}
