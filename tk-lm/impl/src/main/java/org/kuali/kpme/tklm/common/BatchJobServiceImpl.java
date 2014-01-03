778ce14593b11089ce06e9c0af567577fc1bfcfb
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-01-03 22:23:17 +0000

    kpme-3139: changed logic to schedule single employee/supervisor approval batch job for one calendar entry

--- tk-lm/impl/src/main/java/org/kuali/kpme/tklm/common/BatchJobServiceImpl.java
+++ tk-lm/impl/src/main/java/org/kuali/kpme/tklm/common/BatchJobServiceImpl.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.exec.util.MapUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
