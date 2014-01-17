cfbe5cfef6bcafb0594d1a585c6880a97192519d
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-01-17 15:32:09 +0000

    [maven-release-plugin]  copy for tag kpme-2.0.1-RC3

---
+++ tags/kpme-2.0.1-RC3/core/impl/src/main/java/org/kuali/kpme/core/leaveplan/service/LeavePlanService.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.core.leaveplan.service;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.kuali.kpme.core.calendar.entry.CalendarEntry;
+import org.kuali.kpme.core.leaveplan.LeavePlan;
+import org.springframework.cache.annotation.Cacheable;
+
+public interface LeavePlanService {
+    
+    /**
+     * Fetch LeavePlan by id
+     * @param lmLeavePlanId
+     * @return
+     */
+    @Cacheable(value= LeavePlan.CACHE_NAME, key="'lmLeavePlanId=' + #p0")
+    public LeavePlan getLeavePlan(String lmLeavePlanId);
+
+    @Cacheable(value= LeavePlan.CACHE_NAME, key="'leavePlan=' + #p0 + '|' + 'asOfDate=' + #p1")
+    public LeavePlan getLeavePlan(String leavePlan, LocalDate asOfDate);
+
+    @Cacheable(value= LeavePlan.CACHE_NAME, key="'leavePlans=' + T(org.kuali.rice.core.api.cache.CacheKeyUtils).key(#p0) + '|' + 'asOfDate=' + #p1")
+    public List<LeavePlan> getLeavePlans(List<String> leavePlan, LocalDate asOfDate);
+    
+    public boolean isValidLeavePlan(String leavePlan);
+    
+    public List<LeavePlan> getAllActiveLeavePlan(String leavePlan, LocalDate asOfDate);
+    
+    public List<LeavePlan> getAllInActiveLeavePlan(String leavePlan, LocalDate asOfDate);
+
+    List<LeavePlan> getLeavePlans(String leavePlan, String calendarYearStart, String descr, String planningMonths,
+                                  LocalDate fromEffdt, LocalDate toEffdt, String active, String showHistory);
+    
+    boolean isFirstCalendarPeriodOfLeavePlan(CalendarEntry calendarEntry, String leavePlan, LocalDate asOfDate);
+    
+    boolean isLastCalendarPeriodOfLeavePlan(CalendarEntry calendarEntry, String leavePlan, LocalDate asOfDate);
+    
+    public List<LeavePlan> getLeavePlansNeedsCarryOverScheduled(int thresholdDays, LocalDate asOfDate);
+
+    @Cacheable(value= LeavePlan.CACHE_NAME, key="'{getFirstDayOfLeavePlan}' + 'leavePlan=' + #p0 + '|' + 'date=' + #p1")
+    public DateTime getFirstDayOfLeavePlan(String leavePlan, LocalDate date);
+
+    @Cacheable(value= LeavePlan.CACHE_NAME, key="'{getRolloverDayOfLeavePlan}' + 'leavePlan=' + #p0 + '|' + 'date=' + #p1")
+    public DateTime getRolloverDayOfLeavePlan(String leavePlan, LocalDate asOfDate);
+}
---
+++ tags/kpme-2.0.1-RC3/tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/timehourdetail/TimeHourDetailRenderer.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.tklm.time.timehourdetail;
+
+import java.util.*;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.joda.time.LocalDate;
+import org.kuali.kpme.core.earncode.EarnCode;
+import org.kuali.kpme.core.earncode.security.EarnCodeSecurity;
+import org.kuali.kpme.core.job.Job;
+import org.kuali.kpme.core.principal.PrincipalHRAttributes;
+import org.kuali.kpme.core.service.HrServiceLocator;
+import org.kuali.kpme.core.util.HrConstants;
+import org.kuali.kpme.core.util.HrContext;
+import org.kuali.kpme.tklm.api.time.timehourdetail.TimeHourDetailRendererContract;
+import org.kuali.kpme.tklm.leave.service.LmServiceLocator;
+import org.kuali.kpme.tklm.time.service.TkServiceLocator;
+import org.kuali.kpme.tklm.time.timeblock.TimeBlock;
+import org.kuali.kpme.tklm.time.workflow.TimesheetDocumentHeader;
+
+public class TimeHourDetailRenderer implements TimeHourDetailRendererContract {
+    private TimeHourDetail timeHourDetail;
+    private boolean overtimeEarnCode;
+
+    public TimeHourDetailRenderer(TimeHourDetail d) {
+        this.timeHourDetail = d;
+
+        TimeBlock tb = TkServiceLocator.getTimeBlockService().getTimeBlock(timeHourDetail.getTkTimeBlockId());
+        if(tb != null) {
+            List<EarnCode> overtimeEarnCodeObjs = HrServiceLocator.getEarnCodeService().getOvertimeEarnCodes(LocalDate.now());
+            List<String> overtimeEarnCodeStrings = HrServiceLocator.getEarnCodeService().getOvertimeEarnCodesStrs(tb.getBeginDateTime().toLocalDate());
+            List<String> eligibleOvertimeEarnCodeListStrings = new ArrayList<String>();
+
+            Job job = HrServiceLocator.getJobService().getJob(HrContext.getTargetPrincipalId(), tb.getJobNumber(), tb.getEndDateTime().toLocalDate());
+            if(job != null) {
+                for (EarnCode earnCode : overtimeEarnCodeObjs) {
+                    String employee = HrContext.isActiveEmployee() ? "Y" : null;
+                    String approver = HrContext.isApprover() ? "Y" : null;
+                    String payrollProcessor = HrContext.isPayrollProcessor() ? "Y" : null;
+
+                    List<EarnCodeSecurity> securityList = HrServiceLocator.getEarnCodeSecurityService().getEarnCodeSecurityList(job.getDept(), job.getHrSalGroup(), earnCode.getEarnCode(), employee, approver, payrollProcessor, job.getLocation(),
+                            "Y", tb.getEndDateTime().toLocalDate());
+                    if(CollectionUtils.isNotEmpty(securityList)) {
+                        eligibleOvertimeEarnCodeListStrings.add(earnCode.getEarnCode());
+                    }
+                }
+            }
+
+            /*
+            KPME-3029 checks to see if user can make a change to the overtime earncode before flagging it as overtime,
+            by either having > 1 earncode opt or having an opt that is different than the timeHourDetail earncode
+            */
+            if((CollectionUtils.isNotEmpty(eligibleOvertimeEarnCodeListStrings) && CollectionUtils.isNotEmpty(overtimeEarnCodeStrings)) && overtimeEarnCodeStrings.contains(d.getEarnCode()) && (eligibleOvertimeEarnCodeListStrings.size() > 1 || !eligibleOvertimeEarnCodeListStrings.contains(d.getEarnCode()))){
+                setOvertimeEarnCode(true);
+            }
+        }
+    }
+
+    public TimeHourDetail getTimeHourDetail() {
+        return timeHourDetail;
+    }
+
+    public String getTkTimeHourDetailId() {
+        return timeHourDetail.getTkTimeHourDetailId();
+    }
+
+    public String getTitle() {
+        return timeHourDetail.getEarnCode();
+    }
+
+    public String getHours() {
+        return timeHourDetail.getHours().toString();
+    }
+
+    public String getAmount() {
+        return timeHourDetail.getAmount().toString();
+    }
+    
+    public String getHolidayName() {
+		String holidayDesc = "";
+		TimeBlock timeBlock = TkServiceLocator.getTimeBlockService().getTimeBlock(timeHourDetail.getTkTimeBlockId());
+		
+		if ( timeBlock != null ){
+			if(timeBlock.getEarnCode().equals(HrConstants.HOLIDAY_EARN_CODE)) {
+				String documentId = timeBlock.getDocumentId();
+				TimesheetDocumentHeader docHeader = TkServiceLocator.getTimesheetDocumentHeaderService().getDocumentHeader(documentId);
+				PrincipalHRAttributes principalCalendar = HrServiceLocator.getPrincipalHRAttributeService().getPrincipalCalendar(docHeader.getPrincipalId(), timeBlock.getBeginDateTime().toLocalDate());
+				
+				if(principalCalendar != null && StringUtils.isNotEmpty(principalCalendar.getLeavePlan())) {
+					holidayDesc = LmServiceLocator.getSysSchTimeOffService().getSSTODescriptionForDate(principalCalendar.getLeavePlan(), timeBlock.getBeginDateTime().toLocalDate());
+				}
+			}
+		}
+			
+		return holidayDesc;
+	}
+
+	public boolean isOvertimeEarnCode() {
+		return overtimeEarnCode;
+	}
+
+	public void setOvertimeEarnCode(boolean overtimeEarnCode) {
+		this.overtimeEarnCode = overtimeEarnCode;
+	}
+
+}
