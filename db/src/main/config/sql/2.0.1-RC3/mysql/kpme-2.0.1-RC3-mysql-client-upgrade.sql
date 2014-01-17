00fb0d17ec3c207d65c2765700adab9bb0fb55b4
JEREMY HANSON <jjhanso@users.noreply.github.com>
2014-01-17 14:35:55 +0000

    KPME-2.0-RC3 database scripts

---
+++ db/src/main/config/sql/2.0.1-RC3/mysql/kpme-2.0.1-RC3-mysql-client-upgrade.sql
@@ -0,0 +1,10 @@
+--  *********************************************************************
+--  Update Database Script
+--  *********************************************************************
+--  Change Log: src/main/config/db/db.changelog-main-upgrade.xml
+--  Ran at: 1/17/14 9:28 AM
+--  Against: tk@localhost@jdbc:mysql://localhost/tk
+--  Liquibase version: 2.0.5
+--  *********************************************************************
+
+--  no changes
