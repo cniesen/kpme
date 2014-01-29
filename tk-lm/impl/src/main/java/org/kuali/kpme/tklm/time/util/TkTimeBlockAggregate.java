00fa3edf93660fecf934eba092d50ae9268236db
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-01-29 14:45:38 +0000

    kpme-3180: fix for time summary from Jignasha

--- tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/util/TkTimeBlockAggregate.java
+++ tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/util/TkTimeBlockAggregate.java
@@ -480,7 +480,7 @@ public class TkTimeBlockAggregate implements TkTimeBlockAggregateContract {
 						timeBlock.setWorkArea(lb.getWorkArea());
 						timeBlock.setTask(lb.getTask());
 						timeBlock.setEarnCode(lb.getEarnCode());
-						
+						timeBlock.setLeaveDate(lb.getLeaveDate());
 						EarnCode earnCodeObj = HrServiceLocator.getEarnCodeService().getEarnCode(lb.getEarnCode(), lb.getLeaveLocalDate());
 						if(earnCodeObj != null) {
 							timeBlock.setEarnCodeType(earnCodeObj.getEarnCodeType());
