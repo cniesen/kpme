53b8efbac1e44be48b4e5c02f56c7dcc00aa88d6
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-01-30 20:09:09 +0000

    [maven-release-plugin]  copy for tag kpme-2.0.1

---
+++ tags/kpme-2.0.1/core/impl/src/main/java/org/kuali/kpme/core/sys/KpmeModuleConfiguration.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.core.sys;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.rice.krad.bo.ModuleConfiguration;
+import org.kuali.rice.krad.datadictionary.DataDictionaryException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+public class KpmeModuleConfiguration extends ModuleConfiguration {
+    @Override
+    public void setDataDictionaryPackages(List<String> dataDictionaryPackages) {
+        this.trimList(dataDictionaryPackages);
+        //resolve individual xml files
+
+        List<String> ddFiles = new ArrayList<String>();
+        for (String ddPackage : dataDictionaryPackages) {
+            if (ddPackage.contains("*")) {
+                try {
+                    PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+                    Resource[] resources = resolver.getResources(ddPackage);
+                    for (Resource resource : resources) {
+                        String prefix = "classpath:";
+                        if (resource instanceof FileSystemResource) {
+                            ddFiles.add("file:" + ((FileSystemResource)resource).getPath());
+                        } else if (resource instanceof ClassPathResource) {
+                            ddFiles.add("classpath:" + ((ClassPathResource)resource).getPath());
+                        } else {
+                            throw new DataDictionaryException("DD Resource " + resource.getFile().getPath() + " not found");
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new DataDictionaryException("DD Resource " + ddPackage + " not found");
+                }
+            } else {
+                ddFiles.add(ddPackage);
+            }
+        }
+        this.dataDictionaryPackages = ddFiles;
+    }
+}
---
+++ tags/kpme-2.0.1/pm/impl/src/main/java/org/kuali/kpme/pm/positionappointment/PositionAppointment.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.pm.positionappointment;
+
+import org.kuali.kpme.core.bo.HrBusinessObject;
+import org.kuali.kpme.core.institution.Institution;
+import org.kuali.kpme.core.location.Location;
+import org.kuali.kpme.pm.api.positionappointment.PositionAppointmentContract;
+
+public class PositionAppointment extends HrBusinessObject implements PositionAppointmentContract {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String pmPositionAppointmentId;
+	private String positionAppointment;
+	private String description;
+	private String institution;
+	private String location;
+	
+	private Location locationObj;
+	private Institution institutionObj;
+	private boolean history;
+	
+	public boolean isHistory() {
+		return history;
+	}
+
+	public void setHistory(boolean history) {
+		this.history = history;
+	}
+
+	public String getPmPositionAppointmentId() {
+		return pmPositionAppointmentId;
+	}
+
+	public void setPmPositionAppointmentId(String pmPositionAppointmentId) {
+		this.pmPositionAppointmentId = pmPositionAppointmentId;
+	}
+
+	public String getPositionAppointment() {
+		return positionAppointment;
+	}
+
+	public void setPositionAppointment(String positionAppointment) {
+		this.positionAppointment = positionAppointment;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getInstitution() {
+		return institution;
+	}
+
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String getId() {
+		return getPmPositionAppointmentId();
+	}
+
+	@Override
+	public void setId(String pmPositionAppointmentId) {
+		setPmPositionAppointmentId(pmPositionAppointmentId);
+	}
+	
+	/**
+	 * @return the locationObj
+	 */
+	public Location getLocationObj() {
+		return locationObj;
+	}
+
+	/**
+	 * @param locationObj the locationObj to set
+	 */
+	public void setLocationObj(Location locationObj) {
+		this.locationObj = locationObj;
+	}
+
+	/**
+	 * @return the institutionObj
+	 */
+	public Institution getInstitutionObj() {
+		return institutionObj;
+	}
+
+	/**
+	 * @param institutionObj the institutionObj to set
+	 */
+	public void setInstitutionObj(Institution institutionObj) {
+		this.institutionObj = institutionObj;
+	}
+
+	@Override
+	protected String getUniqueKey() {
+		return getPositionAppointment() + "_" + getInstitution() + "_" + getLocation();
+	}
+
+}
