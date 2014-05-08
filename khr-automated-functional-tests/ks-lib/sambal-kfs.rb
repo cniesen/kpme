1ba3d96b7b07372a40e5619eb7dd7ac7b22779d9
JEREMY HANSON <jjhanso@users.noreply.github.com>
2014-05-08 14:55:23 +0000

    removed extra unneeded directories

--- khr-automated-functional-tests/ks-lib/sambal-kfs.rb
+++
@@ -1,9 +0,0 @@
-require 'test-factory'
-
-$: << File.dirname(__FILE__)+'/sambal-khr'
-require 'sambal-kuali/kuali_base_page'
-Dir["#{File.dirname(__FILE__)}/sambal-khr/*.rb"].each {|f| require f }
-#Dir["#{File.dirname(__FILE__)}/sambal-khr/pages/*.rb"].each {|f| require f }
-Dir["#{File.dirname(__FILE__)}/sambal-khr/pages/**/*.rb"].each {|f| require f }
-Dir["#{File.dirname(__FILE__)}/sambal-khr/data_objects/*.rb"].each {|f| require f }
-
