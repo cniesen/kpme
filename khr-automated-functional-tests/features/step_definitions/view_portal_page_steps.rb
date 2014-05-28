7cb4f0aaec191dbc28ceaeab75a9a390016a003d
prakash <prakash@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-05-28 10:29:42 +0000

    Cleaning up the folder structure

--- khr-automated-functional-tests/features/step_definitions/view_portal_page_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^I attempt to view the KHR portal page$/ do
-  visit PortalMenu
-end
-
-#Then /^I should be able to see a portal group named "(.*?)"$/ do |arg1|
-Then /^I should be able to see a portal group named \"Transactions\"$/ do
-  PortalMenu.expected_element :portal_group_transactions
-end
