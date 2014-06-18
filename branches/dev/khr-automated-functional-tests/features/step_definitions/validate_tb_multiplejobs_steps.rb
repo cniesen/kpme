02425f6cec831b976de9ba9d1c653fb1fc43c4db
prakash <prakash@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-06-18 11:51:04 +0000

    KPMEQA-271
    changes to have dynamic parameters in then commands

--- branches/dev/khr-automated-functional-tests/features/step_definitions/validate_tb_multiplejobs_steps.rb
+++ branches/dev/khr-automated-functional-tests/features/step_definitions/validate_tb_multiplejobs_steps.rb
@@ -1,7 +1,7 @@
 When(/^I add multiple time blocks with different assignments$/) do
   navigate_to_timedetail
 
-
+  @days = 1
   on KpmeCalendarPage do |page|
     @finalstart_dt,@finalend_dt = page.calc_dates(1,1)
   end
@@ -11,7 +11,9 @@ When(/^I add multiple time blocks with different assignments$/) do
                       :end_date => @finalend_dt,
                       :assignment => "IA-EC HR Work Area : $5.00 Rcd 0 IA-EC",
                       :earn_code => "EC1 : EC1 - Security",
-                      :hours => "2"
+                      :hours => "2",
+                      :assignment_number => "0"
+
 
   on KpmeCalendarPage do |page|
     @finalstart_dt,@finalend_dt = page.calc_dates(1,1)
@@ -21,8 +23,9 @@ When(/^I add multiple time blocks with different assignments$/) do
                       :end_date => @finalend_dt,
                       :assignment => "IA-EC NE Work Area : $5.00 Rcd 1 IA-EC",
                       :earn_code => "EC1 : EC1 - Security",
-                      :hours => "4",
-                      :defer_check_entry => "true"
+                      :hours => "2",
+                      :defer_check_entry => "true",
+                      :assignment_number => "1"
 
 
 end
@@ -30,12 +33,14 @@ end
 
 Then(/^the different time blocks must appear in summary$/) do
   on KpmeCalendarPage do |page|
-    page.summary_assignment(0).should == @timeblock_hr.assignment
-    page.summary_fieldvalue(0,'EC1',1).should include @timeblock_hr.hours
-    page.summary_assignment_bgcolor(0).should == "rgba(152, 163, 134, 1)"
-    page.summary_assignment(1).should == @timeblock_ne.assignment
-    page.summary_fieldvalue(1,'EC1',1).should include @timeblock_ne.hours
-    page.summary_assignment_bgcolor(1).should == "rgba(204, 204, 153, 1)"
+
+    page.summary_assignment(@timeblock_hr.assignment_number).should == @timeblock_hr.assignment
+    page.summary_fieldvalue(@timeblock_hr.assignment_number,@timeblock_hr.earn_code[0,3],@days).should include @timeblock_hr.hours
+    page.summary_assignment_bgcolor(@timeblock_hr.assignment_number).should == "rgba(152, 163, 134, 1)"
+
+    page.summary_assignment(@timeblock_ne.assignment_number).should == @timeblock_ne.assignment
+    page.summary_fieldvalue(@timeblock_ne.assignment_number,@timeblock_ne.earn_code[0,3],@days).should include @timeblock_ne.hours
+    page.summary_assignment_bgcolor(@timeblock_ne.assignment_number).should == "rgba(204, 204, 153, 1)"
 
   end
 
