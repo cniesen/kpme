e1edd6d04abeb708ce49ca7d9427dde177c6ab5d
prakash <prakash@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-07-17 09:01:45 +0000

    KPMEQA-279

---
+++ khr-automated-functional-tests/features/step_definitions/validate_tb_outside_paycalendar_steps.rb
@@ -0,0 +1,56 @@
+When(/^I select dates prior to the pay calendar$/) do
+
+    on KpmeCalendarPage do |page|
+      @finalstart_dt,@finalend_dt = page.calc_dates(-15,-14)
+    end
+
+    @timeblock = create TimeBlockObject, :start_date => @finalstart_dt,
+                        :end_date => @finalend_dt,
+                        :defer_check_entry => true,
+                        :defer_add => true
+end
+
+
+Then(/^assignment field is selected with no assignments$/) do
+  on TimeblockWidgetPage do |page|
+    page.assignment.selected?("-- no assignments found --").should == true
+  end
+end
+
+
+When(/^I select dates later to the pay calendar$/) do
+  on KpmeCalendarPage do |page|
+    @finalstart_dt,@finalend_dt = page.calc_dates(15,16)
+  end
+
+  @timeblock = create TimeBlockObject, :start_date => @finalstart_dt,
+                      :end_date => @finalend_dt,
+                      :defer_check_entry => true,
+                      :defer_add => true
+end
+
+
+When(/^I select start date earlier to the current pay period$/) do
+
+  on KpmeCalendarPage do |page|
+    @finalstart_dt,@finalend_dt = page.calc_dates(-2,1)
+  end
+
+  @timeblock = create TimeBlockObject, :start_date => @finalstart_dt,
+                      :end_date => @finalend_dt,
+                      :defer_check_entry => true,
+                      :defer_add => true
+end
+
+
+When(/^I select end date later to the current pay period$/) do
+  on KpmeCalendarPage do |page|
+    @finalstart_dt,@finalend_dt = page.calc_dates(12,15)
+  end
+
+  @timeblock = create TimeBlockObject, :start_date => @finalstart_dt,
+                      :end_date => @finalend_dt,
+                      :day_click => 12,
+                      :defer_check_entry => true,
+                      :defer_add => true
+end
