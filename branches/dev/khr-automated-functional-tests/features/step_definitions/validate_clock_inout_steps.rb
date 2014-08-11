80562612f24984020b6f75124ce0847d15e29768
prakash <prakash@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-08-11 13:33:01 +0000

    KPMEQA-49

--- branches/dev/khr-automated-functional-tests/features/step_definitions/validate_clock_inout_steps.rb
+++ branches/dev/khr-automated-functional-tests/features/step_definitions/validate_clock_inout_steps.rb
@@ -1,19 +1,20 @@
 Given(/^I am logged in as indiana hourly time keeping employee$/) do
   log_in 'inclock1'
-  on ClockPage do |page|
-    if (page.work_status.text != "Work Status : No previous clock information")
-      @doc_id=page.document_id
-      log_in 'admin'
-      clear_existing_document(@doc_id)
-      log_in 'inclock1'
-    end
+
+
+ on ClockPage do |page|
+  if (page.work_status.text[0,24] == "Work Status : Clocked in")
+    @doc_id=page.document_id
+    log_in 'admin'
+    clear_existing_document(@doc_id)
+    log_in 'inclock1'
   end
+
+ end
 
 end
 
-
 When(/^I clock in and out hours$/) do
-
   @timeentry = create ClockTimeentryObject, :wait_time=>120
 end
 
@@ -21,15 +22,14 @@ Then(/^time entry should be created in calendar page$/) do
   navigate_to_timedetail
   on KpmeCalendarPage do |page|
 
-    @cal_date=@timeentry.initial_time.to_s[-42,2]
+    @cal_date=@timeentry.clock_in_work_sts.to_s[-42,2]
     @cal_date = @cal_date[1,1] if @cal_date < "10"
-    @date_in=@timeentry.clock_in_work_sts.to_s[-42,2]
+    @date_in=@timeentry.clock_out_work_sts.to_s[-42,2]
     @date_in = @date_in[1,1] if @date_in < "10"
     @clock_in=@timeentry.clock_in_work_sts.to_s[-34,5]
     @in_noon=@timeentry.clock_in_work_sts.to_s[-25,2]
     @clock_out=@timeentry.clock_out_work_sts.to_s[-34,5]
     @out_noon=@timeentry.clock_out_work_sts.to_s[-25,2]
-
 
     page.calendar_text_entry(@cal_date).should include (@date_in)
     page.calendar_text_entry(@cal_date).should include ("IN-DEPT HR Work Area")
@@ -50,7 +50,6 @@ Then(/^I should see all the exact time values$/) do
     calendar_date= Date.strptime(calendar_date, '%a, %B %d %Y %H:%M:%S %p, %Z').strftime('%m/%d/%Y')
     page.windows[1].use
     @list_no=(page.number_of_items)-2
-
 
     page.timeentry_table(@list_no).should include (calendar_date+" "+"IN-DEPT HR Work Area : $5.00 Rcd 0 IN-DEPT"+" "+@clock_in+" "+@in_noon+" "+calendar_date+" "+@clock_in+" "+@in_noon+" "+@clock_out+" "+@out_noon+" "+calendar_date+" "+@clock_out+" "+@out_noon)
     page.windows.last.close
