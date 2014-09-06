51023b8afeca411f798746f02299b9e278dfff8f
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-09-06 05:36:30 +0000

    Automated license header update

--- branches/kpme-2.1/web/src/main/webapp/swipe/app/bower_components/json3/lib/json3.js
+++ branches/kpme-2.1/web/src/main/webapp/swipe/app/bower_components/json3/lib/json3.js
@@ -1,4 +1,18 @@
-/*! JSON v3.2.6 | http://bestiejs.github.io/json3 | Copyright 2012-2013, Kit Cambridge | http://kit.mit-license.org */
+/*
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 ;(function (window) {
   // Convenience aliases.
   var getClass = {}.toString, isProperty, forEach, undef;
