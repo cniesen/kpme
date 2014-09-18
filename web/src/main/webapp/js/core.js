85ada01a2100f4f7fbe5d7f3ec15a3cca182a954
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-09-18 20:06:03 +0000

    kpme-3805: Made Process field readonly after a process selected

--- web/src/main/webapp/js/core.js
+++ web/src/main/webapp/js/core.js
@@ -58,33 +58,6 @@ function employeeProcessChangeSubmitForm(methodToCall) {
     ajaxSubmitForm(methodToCall,additionalData);
 }
 
-function highlightEmployeeProcessRequiredChanges() {
-    if (jQuery('#position_process_control').val() == '') {
-        jQuery('.ui-datepicker-trigger').hide();
-    } else {
-        jQuery('.ui-datepicker-trigger').show();
-    }
-
-    if (jQuery("#position_process_control").val() == "Reorganization") {
-        jQuery("input[name='document.newMaintainableObject.dataObject.primaryDepartment']").css("border","2px solid orange");
-        jQuery("input[name='document.newMaintainableObject.dataObject.reportsToPositionId']").css("border","2px solid orange");
-    } else {
-        jQuery("input[name='document.newMaintainableObject.dataObject.primaryDepartment']").css("border","");
-        jQuery("input[name='document.newMaintainableObject.dataObject.reportsToPositionId']").css("border","");
-    }
-
-    if (jQuery("#position_process_control").val() == "Reclassification") {
-        jQuery("input[name='document.newMaintainableObject.dataObject.positionClass']").css("border","2px solid orange");
-    } else {
-        jQuery("input[name='document.newMaintainableObject.dataObject.positionClass']").css("border","");}
-
-    if (jQuery("#position_process_control").val() == "Change Status") {
-        jQuery("select[name='document.newMaintainableObject.dataObject.positionStatus']").css("border","2px solid orange");
-    } else {
-        jQuery("select[name='document.newMaintainableObject.dataObject.positionStatus']").css("border","");
-    }
-}
-
 //jQuery(document).ready(function(){
 jq(document).ready(function () {
     jq('input[name="document.newMaintainableObject.effectiveDate"]').blur(function() {
