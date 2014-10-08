56c48734a8eb8e114701cdc34968f206656b82bd
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-10-08 14:54:35 +0000

    kpme-3855: Changed Edit link label to Open

--- core/impl/src/main/java/org/kuali/kpme/core/tkmdocument/web/KhrEmployeeDocumentLookupableImpl.java
+++ core/impl/src/main/java/org/kuali/kpme/core/tkmdocument/web/KhrEmployeeDocumentLookupableImpl.java
@@ -15,16 +15,17 @@
  */
 package org.kuali.kpme.core.tkmdocument.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.kuali.kpme.core.lookup.KPMELookupableImpl;
 import org.kuali.kpme.core.util.TKUtils;
 import org.kuali.rice.kim.api.identity.Person;
 import org.kuali.rice.kim.api.services.KimApiServiceLocator;
 import org.kuali.rice.krad.bo.BusinessObject;
+import org.kuali.rice.krad.uif.element.Action;
 import org.kuali.rice.krad.web.form.LookupForm;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by jwillia on 4/11/14.
@@ -48,5 +49,15 @@ public class KhrEmployeeDocumentLookupableImpl extends KPMELookupableImpl {
     public boolean allowsMaintenanceNewOrCopyAction() {
         return false;
     }
+       
+    @Override
+    public void getMaintenanceActionLink(Action actionLink, Object model, String maintenanceMethodToCall) {
+    	// change label for Edit action to Open
+    	if(actionLink.getActionLabel().equals("edit")) {
+    		actionLink.setActionLabel("Open");
+    	}
+    	super.getMaintenanceActionLink(actionLink, model, maintenanceMethodToCall);
+    }
+    
 }
 
